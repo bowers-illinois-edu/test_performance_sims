2db6a967fec08875c322901f3b764c2a5c77274c
Jake Bowers <jake@jakebowers.org>
2024-03-24 11:18:12 -0300

    Touch the .done file in Data

--- Data/make_test_data.R
+++ Data/make_test_data.R
@@ -16,14 +16,18 @@ idat <- data.table(i = 1:nunits, b = rep(c(1:nblocks), length = nunits))
 bdat <- data.table(b = 1:nblocks)
 setkey(bdat, b)
 setkey(idat, b)
+
 ## Make vb1, vb2, vb3 all nest within each other like states, counties, districts (with the individual blocks nested within all three)
 ## 2 levels at highest level
-bdat[, vb1 := rep(c(1, 2), length.out = .N)]
+bdat[, vb1 := rep(1:2, length.out = .N)]
 ## 2 levels within each state
 bdat[, vb2 := rep(1:2, length.out = .N), by = vb1]
-bdat[, vbnest := interaction(vb1, vb2, lex.order = TRUE, drop = TRUE)]
+bdat[, vb3 := rep(1:2, length.out =.N), by=interaction(vb1, vb2, lex.order = TRUE, drop = TRUE)]
 ftable(bdat$vb1, bdat$vb2)
+ftable(bdat$vb1, bdat$vb2,bdat$vb3)
+
 ## Now merge the block data onto the individual level data
+## make potential outcome to control/status quo depend on a covariate at both the individual and block levels.
 idat <- bdat[idat]
 set.seed(12345)
 idat[, vi1 := round(runif(.N, min = 0, max = 10)), by = b]
@@ -54,7 +58,6 @@ idat$gF <- interaction(idat$bF, idat$ZF)
 idat[, Ymd := Y - mean(Y), by = b]
 idat[, Zmd := Z - mean(Z), by = b]
 
-
 ## Observed Effects by Block
 idat[, list(
   canceling = mean(Y[Z == 1] - Y[Z == 0]),
@@ -77,23 +80,35 @@ setkey(bdat, bF)
 ## also unequal tau (causal effects)
 bdat2 <- data.table(nb = rep(seq(4,400,length=100),length=nblocks), bF = factor(1:nblocks))
 table(bdat2$nb)
+
+## Make vb1, vb2, vb3 all nest within each other like states, counties, districts (with the individual blocks nested within all three)
+## 2 levels at highest level
+bdat2[, vb1 := rep(1:2, length.out = .N)]
+## 2 levels within each state
+bdat2[, vb2 := rep(1:2, length.out = .N), by = vb1]
+bdat2[, vb3 := rep(1:2, length.out =.N), by=interaction(vb1, vb2, lex.order = TRUE, drop = TRUE)]
+ftable(bdat2$vb1, bdat2$vb2)
+ftable(bdat2$vb1, bdat2$vb2,bdat2$vb3)
+
 setkey(bdat2, "bF")
 setkey(idat, "bF")
+
 ## Make a dataset with unequal sized blocks
-#idat2 <- bdat2[idat]
-#idat2[,.(unique((.N*nb)/nblocks),nb=unique(nb),N=.N),by=bF]
 set.seed(12345)
-##idat3 <- idat2[, sample(.N, size = unique((.N * nb) / nblocks), replace = TRUE), by = bF])
-idat3 <- data.table(bF=rep(as.character(bdat2$bF),bdat2$nb))
+idat2 <- data.table(bF=rep(as.character(bdat2$bF),bdat2$nb))
+## Add the vb variables to the indiv level data
+idat3 <- bdat2[idat2]
+ftable(idat3$vb1, idat3$vb2,idat3$vb3)
 idat3[,bF:=as.factor(bF)]
 idat3[,nb:=.N,by=bF]
 ## Now giving it the same blocking names as idat for ease with testing
 setkey(idat3, bF)
 idat3 <- idat3[, b := as.numeric(as.character(bF))]
+## Make more indiv level covariates
 idat3[, c("v1", "v2", "v3", "v4") := lapply(1:4, function(i) {
   rnorm(.N, mean = .N / i, sd = 1 / i)
 }), by = bF]
-idat3[, y0 := v1 + rnorm(.N), by = bF]
+idat3[, y0 := v1 + vb1+ rnorm(.N), by = bF]
 idat3[, tau := ifelse(b <= nblocks, -5 * sd(y0), 5 * sd(y0))]
 idat3[, tauhomog := 5 * sd(y0)]
 ## Taunorm_inc increases the effect with the block number and also size of the block.
@@ -115,7 +130,6 @@ nullbF <- sample(unique(levels(idat3$bF)), size = length(unique(idat3$bF)) / 4)
 idat3[, y1norm_inc := ifelse(bF %in% nullbF, y0, y0 + taunorm_inc)]
 idat3[, y1norm_dec := ifelse(bF %in% nullbF, y0, y0 + taunorm_dec)]
 idat3[, y1tauv2 := y0 + tauv2]
-idat3[, nb := .N, by = bF]
 
 ## Randomly assign treatment but allow probability of assignment to vary by block
 set.seed(1235)
@@ -138,7 +152,10 @@ bdat3 <- idat3[, .(
   ate_homog = mean(y1homog - y0),
   ate_norm_inc = mean(y1norm_inc - y0),
   ate_norm_dec = mean(y1norm_dec - y0),
-  ate_tauv2 = mean(y1tauv2 - y0)
+  ate_tauv2 = mean(y1tauv2 - y0),
+  vb1=unique(vb1),
+  vb2=unique(vb2),
+  vb3=unique(vb3)
 ), by = bF]
 bdat3[, hwt := (2 * (nc * nt) / (nc + nt))]
 
@@ -165,4 +182,5 @@ save(idat_not_equal_nb,file=here::here("Data","idat_not_equal_nb.rda"))
 save(bdat_equal_nb,file=here::here("Data","bdat_equal_nb.rda"))
 save(bdat_not_equal_nb,file=here::here("Data","bdat_not_equal_nb.rda"))
 
-system("touch make_test_data.done")
+system("touch Data/make_test_data.done")
+
