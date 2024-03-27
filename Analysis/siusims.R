e08577bdc80b97b06ca7a794020273f8e7c8cef9
Jake Bowers <jake@jakebowers.org>
2024-03-26 22:43:29 -0300

    Format nicely

--- Analysis/siusims.R
+++ Analysis/siusims.R
@@ -7,8 +7,8 @@ library(manytestsr)
 setDTthreads(1)
 
 ## This loads the data files and simulation parameters
-load(here("Data","equal_blocksize_data.rda"),verbose=TRUE)
-load(here("Analysis","simparms.rda"),verbose=TRUE)
+load(here("Data", "equal_blocksize_data.rda"), verbose = TRUE)
+load(here("Analysis", "simparms.rda"), verbose = TRUE)
 RNGkind("L'Ecuyer-CMRG")
 set.seed(12345)
 nsims <- 1000
@@ -17,7 +17,7 @@ simparms$nsims <- nsims
 ## For speed, break the jobs into those with fewer blocks versus more blocks.
 ## This should let us catch mistakes in code more quickly
 ## Also focus on smaller datasets for now and for now also just equal sized blocks
-simparms <- droplevels(simparms[nblocks < 1000 & block_sizes=="equal", ])
+simparms <- droplevels(simparms[nblocks < 1000 & block_sizes == "equal", ])
 rm(list = ls(patt = "B1000"))
 
 ## Now set up the cluster environment
@@ -28,7 +28,7 @@ source(here::here("Analysis", "setup-clusters.R"), verbose = TRUE, echo = TRUE)
 print(clusterswitch)
 cl_exists <- exists("cl")
 blah <- plan()
-plan_exists <- !inherits(try(class(blah)),"try-error")
+plan_exists <- !inherits(try(class(blah)), "try-error")
 
 ## Sometimes here() has problems with multi-node parallel processing
 ## So try another way outside of the loop
@@ -46,9 +46,9 @@ test_and_summarize_fn <- function(i) {
     set.seed(12345)
     x <- simparms[i, ]
     ## nmsx0 <- names(x)
-    ##nmsx <- grep('unequal',nmsx0,invert=TRUE,value=TRUE)
-    ##xnm <- paste(x[,.SD,.SDcols=nmsx], collapse = "_")
-    xnm <- paste(x,collapse="_")
+    ## nmsx <- grep('unequal',nmsx0,invert=TRUE,value=TRUE)
+    ## xnm <- paste(x[,.SD,.SDcols=nmsx], collapse = "_")
+    xnm <- paste(x, collapse = "_")
     message(xnm)
     ptm <- proc.time()
     p_sims_tab <- padj_test_fn(
@@ -80,7 +80,7 @@ test_and_summarize_fn <- function(i) {
     return(res)
 }
 
-#if (clusterswitch == "keeling-socket") {
+# if (clusterswitch == "keeling-socket") {
 if (cl_exists & !plan_exists) {
     clusterEvalQ(cl, library(here))
     clusterEvalQ(cl, library(manytestsr))
@@ -107,18 +107,18 @@ if (file.exists(here("Analysis", "not_done_idx.rda"))) {
 ## if any of them are really great at not leaving cores unused while the main
 ## process waits for things to finish.
 
-#if (clusterswitch == "keeling=-socket") {
+# if (clusterswitch == "keeling=-socket") {
 if (cl_exists & !plan_exists) {
     p_sims_res <- parLapplyLB(cl, theidx,
         fun = function(i) {
             test_and_summarize_fn(i)
-        },chunk.size=1
+        }, chunk.size = 1
     )
     stopCluster(cl)
 }
 
 # if (clusterswitch == "keeling=-future") {
-if (plan_exists){
+if (plan_exists) {
     ## here we are trying to load balance since each set of parameters can require a very different amount of time to run
     options(future.globals.maxSize = +Inf)
     options(future.wait.interval = 0L) # .001)
@@ -132,17 +132,19 @@ if (plan_exists){
         future.packages = c("here", "manytestsr"),
         future.globals = c("test_and_summarize_fn", globals),
         future.scheduling = Inf,
-        future.chunk.size=NULL
+        future.chunk.size = NULL
     )
 
     plan(sequential)
 }
+
+test_and_summarize_fn(162)
 ##  This next for debugging
 ## p_sims_res <- mclapply(theidx,
 ##     FUN = function(i) {
 ##         message(i)
 ##         test_and_summarize_fn(i)
-##     }, mc.cores = 12
+##     }, mc.cores = 12, mc.preschedule = FALSE
 ## )
 
 ## p_sims_res <- lapply(theidx,
@@ -156,5 +158,5 @@ if (plan_exists){
 ## These next are not used since it takes days to run the simulation, at least for now.
 ## But we leave them here since maybe they will be used at some other time
 save(p_sims_res, file = here::here("Analysis", "p_sims_res_equal_nb_2024.rda"))
-#siusimsres_notB1000 <- rbindlist(p_sims_res, idcol = TRUE)
-#save(siusimsres_notB1000, file = here::here("Analysis", "siusimsres-notB1000_2024.rda"))
+# siusimsres_notB1000 <- rbindlist(p_sims_res, idcol = TRUE)
+# save(siusimsres_notB1000, file = here::here("Analysis", "siusimsres-notB1000_2024.rda"))
