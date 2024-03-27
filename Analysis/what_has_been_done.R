8cf9d94542101ec1cbf4876f001f15d7fdc441ab
Jake Bowers <jake@jakebowers.org>
2024-03-27 08:21:46 -0300

    Update how to make the not_done_idx list

--- Analysis/what_has_been_done.R
+++ Analysis/what_has_been_done.R
@@ -4,12 +4,15 @@ library(stringi)
 library(here)
 load(here("Analysis", "simparms.rda"))
 
+## Relies on simparms being keyed to idx
+stopifnot(key(simparms) == "idx")
+
 ## We have been doing 1000 sims
 simparms[, nms := do.call(paste, c(.SD, "1000", sep = "_"))]
 
 done <- stri_replace_all_fixed(list.files(path = here("Analysis/CSVS"), patt = "[0-9]*csv"), replacement = "", pattern = ".csv", vectorize_all = TRUE)
 
-idx <- seq_len(nrow(simparms))
-done_idx <- which(simparms$nms %in% done)
+idx <- simparms$idx # seq_len(nrow(simparms))
+done_idx <- simparms$idx[simparms$nms %in% done]
 not_done_idx <- setdiff(idx, done_idx)
 save(not_done_idx, file = here("Analysis", "not_done_idx.rda"))
