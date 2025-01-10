0f5047a6293d07a34f0e8083cc5a48e6ccfa6ee7
Jake Bowers <jake@jakebowers.org>
2025-01-09 22:15:09 -0600

    Working on assessing strong control

---
+++ Analysis/simple_error_rate_sim_v2_strong_control.R
@@ -0,0 +1,209 @@
+## The idea here is to directly generate p_values from either a uniform
+## distribution (for hypothese that are true ) and p_values from a beta
+## distribution for hypotheses that are false.
+
+## We know that the total number of tests in a tree is
+## sum(num_nodes^seq(0,num_levels)). For example, in a tree with 2 nodes per
+## level and 3 levels we have 2^0+2^1+2^2+2^3=1+2+4+8=15 tests. Now, which
+## tests should be false? We could randomly choose some proportion prop_false
+## out of the total. Some of them will be at the higher levels and some the
+## lower levels. We could also do this by flipping a coin for each tests: is
+## this a true test versus a false test.
+
+## Alternatively we could set prop_false for the proportion false among the
+## leave node hypotheses (which are num_nodes^num_levels in number). And then
+## any parent_p between that child/leaf node and the root is also false (i.e.
+## draws from rbeta rather than runif).
+
+
+library(here)
+library(abind)
+library(tidyverse)
+library(parallel)
+
+
+# Function to generate Beta random variables with minimum 'a' and maximum 'c'
+beta_with_min <- function(n, alpha, beta, a, c) {
+  # Generate standard Beta random variables
+  x <- rbeta(n, shape1 = alpha, shape2 = beta)
+  # Transform to desired range
+  y <- a + (c - a) * x
+  return(y)
+}
+
+# Example usage
+set.seed(123)
+samples <- beta_with_min(
+  n = 1000, # Number of samples
+  alpha = .1, # Shape parameter 1
+  beta = 1, # Shape parameter 2
+  a = 0, # Minimum value
+  c = 1 # Maximum value
+)
+summary(samples)
+plot(density(samples))
+
+
+assign_false_H <- function(nodes_per_level, num_levels, prop_false) {
+  # Calculate total number of leaves
+  num_leaves <- nodes_per_level^(num_levels - 1)
+
+  # Randomly select leaves to have false null hypotheses of no effects (i.e. leaves to have actual treatment effects)
+  num_selected <- floor(prop_false * num_leaves)
+  selected_leaves <- sample(1:num_leaves, num_selected)
+
+  # Create a logical vector for all nodes
+  total_nodes <- (nodes_per_level^num_levels - 1) / (nodes_per_level - 1)
+  has_false_hyp <- logical(total_nodes)
+
+  ## TODO START HERE
+  lvl_nms <- character(total_nodes)
+  lvl_nms[1] <- "lv_0"
+  lvl_nms[2:(nodes_per_level + 1)] <- paste("lv", 1:nodes_per_level, sep = "_")
+  for (i in seq(3, (num_levels + 1))) {
+    lvl_nms[(nodes_per_level + i - 1):(i + nodes_per_level)] <- paste(lvl_nms[i - 1], 1:nodes_per_level, sep = "_")
+  }
+  lvl_nms
+
+  lvl_nms <- c("lv_0", lvl_nms)
+  names(has_false_hyp) <- lvl_nms
+
+  # Mark selected leaves
+  ## Among the total nodes, which ones are the leaves and which the parents?
+  leaf_start <- total_nodes - num_leaves + 1
+  has_false_hyp[leaf_start + selected_leaves - 1] <- TRUE
+
+  # Propagate up the tree
+  for (level in (num_levels - 1):1) {
+    level_start <- (nodes_per_level^(level - 1) - 1) / (nodes_per_level - 1) + 1
+    level_end <- (nodes_per_level^level - 1) / (nodes_per_level - 1)
+
+    for (node in level_start:level_end) {
+      # Check if any child has characteristic A
+      child_start <- node * nodes_per_level - (nodes_per_level - 1)
+      child_end <- node * nodes_per_level
+      has_false_hyp[node] <- any(has_false_hyp[child_start:child_end])
+    }
+  }
+
+  return(has_false_hyp)
+}
+
+
+test1 <- assign_false_H(nodes_per_level = 2, num_levels = 3, prop_false = .5)
+
+
+#' Simulate P-values on a tree where some nodes have treatment effects
+#' (hypotheses are false) and others have true nulls (no treatment effects)
+#'
+#' Specify Generate one p-value at the root
+#' level. Then generate J p-values, one for each of j=1...J child nodes ---
+#' where each node has a true null of no effects and where the minimum reported
+#' p-value for each node under the root node is the minimum p-value <= alpha
+#' and the maximum is 1. Do this for each level. Here, assuming the same number
+#' of nodes per level throughout the tree below the root level. For now, we
+#' assume that there are no false nulls. This is a completely no effects world.
+#' should make .05 of values less than .05
+sim_ps <- function(num_levels, nodes_per_level, alpha, return_only_errors =
+                     FALSE, test_limit = 1e+6) {
+  stopifnot(nodes_per_level >= 2)
+  stopifnot(num_levels >= 2)
+
+  ## Number of nodes in a k-ary tree: canonical formula has k nodes per level and n levels
+  ## tot_tests_1 <- sum(nodes_per_level^seq(0, (num_levels-1)))
+  tot_tests_2 <- ((nodes_per_level^num_levels) - 1) / (nodes_per_level - 1)
+  stopifnot(tot_tests_1 == tot_tests_2)
+
+  ## This generates p-values from a uniform distribution (i.e. all hypotheses are true)
+  ## for each parent node (ex. given p_0 and anumber of nodes at level 1=2,
+  ## this generates two p_values labeled "p_1" with both of their minimum of
+  ## p_0)
+  get_child_ps <- function(parent_p, num_nodes = nodes_per_level, depth) {
+    node_ps <- sapply(1:num_nodes, function(i) {
+      thep <- runif(1, min = parent_p, max = 1)
+      res <- c(thep, parent_p)
+      names(res) <- c(
+        paste("p_", depth, sep = ""),
+        paste("p_", depth - 1, sep = "")
+      )
+      return(res)
+    })
+    return(t(node_ps))
+  }
+
+  ## Start with the root node.
+  p_0 <- runif(1, min = 0, max = 1)
+  sim_id_level_0 <- "0"
+
+  ## for now, require at least a root node and 1 set of child nodes
+  stopifnot(num_levels > 0)
+
+  ## Setup a list to receive the data from the other levels
+  res_list <- vector("list", length = num_levels)
+
+  ## Then do the first level after the root by hand
+  p_1_dt <- as_tibble(get_child_ps(parent_p = p_0, depth = 1))
+  p_1_dt$sim_id_level_0 <- rep(sim_id_level_0, each = nodes_per_level)
+  p_1_dt$sim_id_level_1 <- paste(p_1_dt$sim_id_level_0, rep(1:nodes_per_level), sep = ".")
+
+  if (!return_only_errors) {
+    ## Setup a list to receive the data from the other levels
+    res_list <- vector("list", length = num_levels)
+    res_list[[1]] <- p_1_dt
+
+    for (i in 2:num_levels) {
+      res <- lapply(res_list[[i - 1]][[paste0("p_", i - 1)]], function(parent_p) {
+        as_tibble(get_child_ps(parent_p = parent_p, depth = i))
+      })
+      res_dt <- dplyr::bind_rows(res)
+      res_dt[[paste0("sim_id_level_", i - 1)]] <- rep(res_list[[i - 1]][[paste0("sim_id_level_", i - 1)]],
+        each = nodes_per_level
+      )
+      res_dt[[paste0("sim_id_level_", i)]] <- paste(res_dt[[paste0("sim_id_level_", i - 1)]],
+        rep(1:nodes_per_level),
+        sep = "."
+      )
+      res_list[[i]] <- res_dt
+      rm(res, res_dt)
+    }
+
+    ## A function to allow the use of the reduce strategy to merge multiple datasets stored in a list together in order.
+    my_join <- function(xdat, ydat) {
+      xdat_id <- grep("^sim_id_level_[0-9]", names(xdat), value = TRUE)
+      ydat_id <- grep("^sim_id_level_[0-9]", names(ydat), value = TRUE)
+      stopifnot(nrow(xdat) >= nrow(ydat))
+      left_join(xdat, ydat, by = intersect(xdat_id, ydat_id), suffix = c("", ".y"))
+    }
+
+    p_dt <- purrr::reduce(res_list[length(res_list):1], my_join)
+    ## Just a few tests
+    stopifnot(all(p_dt$p_1 >= p_dt$p_0))
+    stopifnot(all.equal(p_dt$p_1.y, p_dt$p_1))
+    ## Drop all columns that are duplicated
+    p_dt <- p_dt %>% select(-ends_with(".y"))
+
+    return(p_dt)
+  } else {
+    tot_tests_1 <- sum(nodes_per_level^seq(0, num_levels))
+    tot_tests_2 <- ((nodes_per_level^num_levels) - 1) / (nodes_per_level - 1)
+    stopifnot(tot_tests_1 == tot_tests_2)
+
+    stopifnot("You have asked for too many tests" = tot_tests_2 < test_limit)
+
+    fp_vec <- rep(NA, length = num_levels + 1)
+    names(fp_vec) <- paste0("lv_", seq(0, num_levels))
+    fp_vec["lv_0"] <- as.numeric(p_0 <= alpha)
+    fp_vec["lv_1"] <- as.numeric(any(p_1_dt$p_1 <= alpha))
+
+    for (i in 2:num_levels) {
+      res <- lapply(res_list[[i - 1]][[paste0("p_", i - 1)]], function(parent_p) {
+        as_tibble(get_child_ps(parent_p = parent_p, depth = i))
+      })
+      res_dt <- dplyr::bind_rows(res)
+      fp_vec[names(fp_vec)[i + 1]] <- as.numeric(any(res_dt[[paste0("p_", i)]] <= alpha))
+      rm(res, res_dt)
+    }
+    return(fp_vec)
+  }
+  ## End
+}
